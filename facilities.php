51c14623220a2d01759444d59b0b301573e46a01
Khyathi <149141359+KhyathiKulal@users.noreply.github.com>
2024-06-21 18:23:12 +0530

    Initial commit

---
+++ facilities.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Deepa Comforts - Facilities</title>
+    <?php require('inc/links.php') ?>
+    <style>
+      .pop:hover{
+        border-top-color: var(--teal) !important; /* increaing priority */
+        transform: scale(1.03);
+        transition: all 0.3s
+      }
+    </style>
+</head>
+ <body class="bg-light">
+   
+    <!-- header attached -->
+    <?php require('inc/header.php') ?>
+
+    <div class="my-5 px-4">
+      <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+      <div class="h-line bg-dark"></div>
+      <p class="text-center mt-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br> Alias, soluta.     Pariatur iste alias fuga accusamus ut laudantium natus dignissimos ipsum
+      </p>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+              <img src="images/wifi.svg" width="50px" >
+              <h5 class="m-0 ms-3">WiFi</h5>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum exercitationem ratione consectetur porro, dolore ipsa</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+              <img src="images/ac.svg" width="50px" >
+              <h5 class="m-0 ms-3">AC</h5>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum exercitationem ratione consectetur porro, dolore ipsa</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+              <img src="images/bed.svg" width="50px" >
+              <h5 class="m-0 ms-3">Bed</h5>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum exercitationem ratione consectetur porro, dolore ipsa</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+              <img src="images/oven.svg" width="50px" >
+              <h5 class="m-0 ms-3">Microwave</h5>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum exercitationem ratione consectetur porro, dolore ipsa</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+              <img src="images/fire.svg" width="50px" >
+              <h5 class="m-0 ms-3">Emergency</h5>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum exercitationem ratione consectetur porro, dolore ipsa</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+              <img src="images/tele.svg" width="50px" >
+              <h5 class="m-0 ms-3">Television</h5>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum exercitationem ratione consectetur porro, dolore ipsa</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      
+    <!-- footer attached -->
+    <?php require('inc/footer.php') ?>
+
+ </body>
+</html>
